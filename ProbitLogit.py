6d99f2066a6817aaf987eba0d627be50caae772a
Daniel Coates <dcoates@selab.(none)>
2012-01-24 16:22:03 -0800

    Initial port of Stan's code.

---
+++ ProbitLogit.py
@@ -0,0 +1,27 @@
+import scipy.special # for erf
+import numpy as np
+
+def ProbitLogit(param,stim, Obs, N,  lower_asymptote, ProbitOrLogit, ChisqOrLL):
+#stim are the stimulus levels
+#Obs is the Observed data
+#N is the number of trials at each level
+#param(1) is the JND, param(2) is PSEs (in stim units)
+# NOTE: above comment is wrong. first param is PSE, second is JND. drc + pi
+    upper_asymptote=1; 
+    z=(stim-param[0])*param[1];
+    if ProbitOrLogit==1:
+        probFull=.5*(scipy.special.erf(z/sqrt(2))+1);#psychometric function going from 0 to 1
+    else:
+        probFull=1/(1+np.exp(-z));
+    end
+    prob = lower_asymptote + (upper_asymptote-lower_asymptote)*probFull;
+    Expect=prob*N;
+    if ChisqOrLL==1:
+        LogLik = -2*sum((Obs*np.log(Expect/(Obs+finfo(float).eps)) +(N-Obs)*np.log((N-Expect)/(N-Obs+finfo(float).eps))));
+    else:
+        Chisq= sum((Obs-Expect)**2/Expect/(1-prob));
+    
+    return [LogLik, prob] 
+#clg;hold off;
+#plot(stim,Obs./N,'x',sim,prob,'-')
+#xlabel('stimulus'); ylabel('prob correct')
