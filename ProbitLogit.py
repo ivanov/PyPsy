f0a31e9c456aff62762d5906a860026724c35731
Dan Coates <daniel.coates@berkeley.edu>
2012-01-31 18:47:50 -0800

    trivial (frustrating) Stan changes

--- ProbitLogit.py
+++ ProbitLogit.py
@@ -9,18 +9,29 @@ def ProbitLogit(param,stim, Obs, N,  lower_asymptote, ProbitOrLogit, ChisqOrLL):
 # NOTE: above comment is wrong. first param is PSE, second is JND. drc + pi
     upper_asymptote=1; 
     z=(stim-param[0])*param[1];
+
+    if ProbitOrLogit>2:
+        param[1]=1./param[1]; #param(2) is now like JND rather than slope (in stim units)
+        ProbitOrLogit=ProbitOrLogit-2; #To allow it to be like before
+    print (param[0], param[1])
+
     if ProbitOrLogit==1:
         probFull=.5*(scipy.special.erf(z/np.sqrt(2))+1);#psychometric function going from 0 to 1
     else:
         probFull=1/(1+np.exp(-z));
     prob = lower_asymptote + (upper_asymptote-lower_asymptote)*probFull;
-    Expect=prob*N;
+
+    if ChisqOrLL>0:
+        Expect=prob*N
+
     if ChisqOrLL==1:
         LogLik = -2*sum((Obs*np.log(Expect/(Obs+np.finfo(float).eps))
             +(N-Obs)*np.log((N-Expect)/(N-Obs+np.finfo(float).eps))));
-    else:
-        Chisq= sum((Obs-Expect)**2/Expect/(1-prob));
-    
+    elif ChisqOrLL==2:
+        LogLik= sum((Obs-Expect)**2./Expect/(1.-prob))
+    elif ChisqOrLL==0:
+        LogLik=0;  #for plotting
+
     return [LogLik, prob] 
 #clg;hold off;
 #plot(stim,Obs./N,'x',sim,prob,'-')
