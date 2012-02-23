363449a91925443dd265aad826a8077e14213aef
Paul Ivanov <pivanov314@gmail.com>
2012-02-22 22:56:32 -0800

    spent way too much time on dprint
    but now it is documented, and even works with:
    
        nosetests pypsy --with-doctest
    
    though I'm not sure why doctests aren't running by default when you do:
    
        python -c "import pypsy; pypsy.test()"

--- pypsy/utils.py
+++ pypsy/utils.py
@@ -4,16 +4,60 @@ import scipy.special as special
 np.set_printoptions(suppress=True)
 
 def dprint(d=None,**kwargs):
-    "Pretty-print a dictionary"
+    """Pretty-print a dictionary.
+
+    You can also pass additional keyword arguments which will get placed in
+    the dictionary that's printed.
+
+    Examples
+    --------
+    >>> mat2d = np.atleast_2d(np.arange(0,1,.2)).T
+    >>> array1d = np.arange(0,1,.3333) - .00000001
+    >>> d=dict(a_really_long_name=mat2d, a=1.3, b=.33, x=.2, bar=array1d)
+    >>> print d # random: dictionaries have no guaranteed order
+    {'a': 1.3, 'x': 0.20000000000000001, 'a_really_long_name': array([[ 0. ],
+           [ 0.2],
+           [ 0.4],
+           [ 0.6],
+           [ 0.8]]), 'b': 0.33000000000000002, 'bar': array([-0.00000001,  0.33329999,  0.66659999,  0.99989999])}
+    >>> dprint(d)
+    a = 1.3
+    a_really_long_name = [[ 0. ]
+                          [ 0.2]
+                          [ 0.4]
+                          [ 0.6]
+                          [ 0.8]]
+    b = 0.33
+    bar = [-0.00000001  0.33329999  0.66659999  0.99989999]
+    x = 0.2
+
+    >>> dprint(d, another_var='hello')
+    a = 1.3
+    a_really_long_name = [[ 0. ]
+                          [ 0.2]
+                          [ 0.4]
+                          [ 0.6]
+                          [ 0.8]]
+    another_var = "hello"
+    b = 0.33
+    bar = [-0.00000001  0.33329999  0.66659999  0.99989999]
+    x = 0.2
+    """
     if d is None:
         d = {}
     d.update(kwargs)
 
-    for k,v in d.items():
-        if np.squeeze(v).shape == ():
-            print k, ' = %.4g'% v
+    for k,v in sorted(d.items()):
+        if isinstance(v, basestring):
+            print k, '= "%s"'% v
+        elif np.squeeze(v).shape == ():
+            print k, '= %.4g'% v
         else:
-            print k, ' = \n', v
+            line = k + ' ='
+            vrep = str(v)
+            # pad every line with space to match the name
+            vlines = ("\n" + " " *len(line) + " ").join(vrep.splitlines())
+            print line, vlines
 
 def ztop(z):
     p = 0.5 + 0.5*special.erf(z/np.sqrt(2))
